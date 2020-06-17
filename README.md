ff0cb7fc0a6c8a95ac808c1c60204b1103b97bc5
Ailton Oliveira <ailton.oliveira@vocacao.org.br>
2020-06-17 12:42:17 -0300

    challenge resolution

---
+++ README.md
@@ -0,0 +1,24 @@
+## GoStack Challenges - Level 01
+Project developed during Rocketseat GoStack, Level 1 challenge on reactjs concepts.
+
+<img alt="ecoleta" src="https://repository-images.githubusercontent.com/272850447/8ff57580-b097-11ea-9219-aff9264488e0" />
+
+## :trophy: Technologies
+
+This project was developed with the following technologies:
+
+- [React JS](https://reactjs.org)
+- [Node JS](https://nodejs.org/en/)
+- [Express](https://expressjs.com/)
+
+## :memo: License
+
+This project is under the MIT license. See the file [LICENSE](LICENSE) for more details.
+
+## :space_invader: Installation
+
+Run yarn to install the dependencies.
+
+---
+
+Developed by Ailton Oliveira :hear_no_evil::speak_no_evil::see_no_evil:
--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    let response = await api.post('repositories', {
+      title: `reactjs-concepts - ${Date.now()}`,
+      url: 'https://github.com/ailtoneric/nodejs-concepts',
+      techs: [
+        'NodeJS', 'ReactJS', 'React Native'
+      ]
+    });
+    
+    let repository = response.data;
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    let response = await api.delete(`repositories/${id}`);
+    if (response.status === 204) {
+
+      let repositoriesFiltered = repositories.filter(repository => repository.id !== id);
+            
+      setRepositories(repositoriesFiltered);
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {
+          repositories.map(repository => (
+            <li key={repository.id}>
+              {repository.title}
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>
+          ))
+        }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
